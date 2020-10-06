ee75645bb6ddb1423b2214cc77f03db2e9e4dfd1
Gayan Virajith <gayanvirajith@gmail.com>
2020-10-06 18:39:15 +0530

    SWA-1232: new config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   plugins: ['commitlint-plugin-jira-rules'],
   extends: ['jira'],
+  rules: {
+    'references-empty': [2, 'never']
+  },
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ['SWA-', 'SWA2-']
+    }
+  }
 }
